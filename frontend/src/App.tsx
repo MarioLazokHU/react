ed25cb384968c4d6a5647fd8576d01174495d7f3
Cynthia <lazokmarios7@gmail.com>
2024-06-27 18:14:31 +0200

    feat: add authentication

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,33 @@
 import { useContext, useEffect, useState, ChangeEvent } from "react";
 import { Button, TextField, Typography } from "@mui/material";
-import { AuthContext } from "./main"; 
+import { AuthContext } from "./main";
 import { getCookie } from "./utils/cookies";
-import checkUser from "./utils/checkUser";
 
 const App = () => {
   const [registered, setRegistered] = useState(false);
   const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("")
-  const [email, setEmail] = useState('')
-  const { isAuthenticated, login, logout } = useContext(AuthContext); 
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const { isAuthenticated, login, logout } = useContext(AuthContext);
 
   useEffect(() => {
-    const validateUser = async () => {
-      const userCookie = getCookie("user");
-      if (userCookie) {
-        const validUser = await checkUser(userCookie.token);
-        if (validUser.id) {
-          login(); 
-        } else {
-          logout();
-        }
+    const checkUser = async (token: string) => {
+      const req = await fetch(`http://localhost:3000/users/get-user/${token}`);
+      const res = await req.json();
+      if (res && res.token && new Date(res.expired).getTime() > Date.now()) {
+        login();
       } else {
         logout();
       }
     };
+    const validateUser = async () => {
+      const userCookie = getCookie("user");
+      if (userCookie) {
+        checkUser(userCookie.token);
+      }
+    };
     validateUser();
-  }, [login, logout]);
+  }, []);
 
   const registerUser = async () => {
     if (username) {
@@ -35,34 +36,32 @@ const App = () => {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ username: username, password: password, email: email }),
+        body: JSON.stringify({ username, password, email }),
       });
       const res = await req.json();
 
       if (res.username) {
         document.cookie = `user=${JSON.stringify(res)}`;
-        login(); 
+        login();
       }
     }
   };
 
-  const logInUser = async (email: string) => {
-    if (email) {
-      const req = await fetch("http://localhost:3000/users/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ password: password, email: email  }),
-      });
-      const res = await req.json();
+  const logInUser = async () => {
+    const req = await fetch("http://localhost:3000/users/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ password, email }),
+    });
+    const res = await req.json();
 
-      if (res && res.username) {
-        document.cookie = `user=${JSON.stringify(res)}`;
-        login();
-      } else if (res && res.error) {
-        alert(res.error);
-      }
+    if (res && res.username) {
+      document.cookie = `user=${JSON.stringify(res)}`;
+      login();
+    } else if (res && res.error) {
+      alert(res.error);
     }
   };
 
@@ -80,44 +79,67 @@ const App = () => {
         <div className="flex flex-col shadow-lg bg-slate-400 w-full p-5 justify-center items-center gap-5">
           <Typography variant="h2">Register User</Typography>
           <TextField
-            onChange={(e: ChangeEvent<HTMLInputElement>) => setUsername(e.target.value)}
+            onChange={(e: ChangeEvent<HTMLInputElement>) =>
+              setUsername(e.target.value)
+            }
             variant="standard"
             label="Username"
+            autoComplete="new-password"
           />
-           <TextField
-            onChange={(e: ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
+          <TextField
+            onChange={(e: ChangeEvent<HTMLInputElement>) =>
+              setEmail(e.target.value)
+            }
             variant="standard"
             label="Email"
+            type="email"
+            autoComplete="new-password"
           />
-           <TextField
-            onChange={(e: ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
+          <TextField
+            onChange={(e: ChangeEvent<HTMLInputElement>) =>
+              setPassword(e.target.value)
+            }
             variant="standard"
             label="Password"
+            type="password"
+            autoComplete="new-password"
           />
           <Button onClick={() => registerUser()} variant="contained">
             Register
           </Button>
-          <Button onClick={() => setRegistered(true)}>You have an account?</Button>
+          <Button onClick={() => setRegistered(true)}>
+            You have an account?
+          </Button>
         </div>
       );
     } else {
       content = (
         <div className="flex flex-col shadow-lg bg-slate-400 w-full p-5 justify-center items-center gap-5">
           <Typography variant="h2">Login</Typography>
-           <TextField
-            onChange={(e: ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
+          <TextField
+            onChange={(e: ChangeEvent<HTMLInputElement>) =>
+              setEmail(e.target.value)
+            }
             variant="standard"
             label="Email"
+            type="email"
+            autoComplete="new-password"
           />
-           <TextField
-            onChange={(e: ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
+          <TextField
+            onChange={(e: ChangeEvent<HTMLInputElement>) =>
+              setPassword(e.target.value)
+            }
             variant="standard"
             label="Password"
+            type="password"
+           autoComplete="new-password"
           />
-          <Button onClick={() => logInUser(email)} variant="contained">
+          <Button onClick={() => logInUser()} variant="contained">
             LogIn
           </Button>
-          <Button onClick={() => setRegistered(false)}>You haven't account?</Button>
+          <Button onClick={() => setRegistered(false)}>
+            You haven't account?
+          </Button>
         </div>
       );
     }
